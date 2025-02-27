534c42193342b1e51b12a4584b6d622e9c6fcb37
Alex <1kimalexander@gmail.com>
2025-02-27 16:13:49 +0400

    Add Pokemon models

--- pokemon_entities/models.py
+++ pokemon_entities/models.py
@@ -1,3 +1,5 @@
+# from django.contrib.gis.gdal.prototypes.raster import auto_create_warped_vrt
 from django.db import models  # noqa F401
 
-# your models here
+class Pokemon(models.Model):
+    title = models.CharField(max_length=200)
