a3d1b4dd80326a82172a96ef23dae72d312f8f9e
Alex <1kimalexander@gmail.com>
2025-03-02 00:44:16 +0400

    Add relation to db models.

--- pokemon_entities/admin.py
+++ pokemon_entities/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Pokemon
+from .models import Pokemon, PokemonEntity
 
 class PokemonAdmin(admin.ModelAdmin):
     list_display = ("title","id")
 
+class PokemonEntityAdmin(admin.ModelAdmin):
+    list_display = ("pokemon", "lat",'lon')
+
 admin.site.register(Pokemon,PokemonAdmin)
+admin.site.register(PokemonEntity,PokemonEntityAdmin)
