754a8bd8651f5a42efae173933410932f5c47b72
Alex <1kimalexander@gmail.com>
2025-03-25 12:14:29 +0400

    Replaced try/except with get_object_or_404 in show_pokemon

--- pokemon_entities/views.py
+++ pokemon_entities/views.py
@@ -2,7 +2,7 @@ import folium
 import json
 
 from django.http import HttpResponseNotFound
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.utils.timezone import localtime
 from .models import Pokemon, PokemonEntity
 
@@ -60,10 +60,7 @@ def show_all_pokemons(request):
 
 def show_pokemon(request, pokemon_id):
     now_local = localtime()
-    try:
-        pokemon = Pokemon.objects.get(pk=pokemon_id)
-    except Pokemon.DoesNotExist:
-        return HttpResponseNotFound('<h1>Такой покемон не найден</h1>')
+    pokemon = get_object_or_404(Pokemon,pk=pokemon_id)
 
     requested_pokemon = pokemon
     previous_evolution = pokemon.prev.first()
